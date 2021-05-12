e4795751789ed0a8fe1503a7c609bfa92e776fc5
Jiwon Lee <65817334+easy-Support@users.noreply.github.com>
2021-05-12 19:28:16 +0900

    Update main.py

--- 11399/main.py
+++ 11399/main.py
@@ -1,6 +1,6 @@
-count = int(input())
-time = list(map(int, input().split()))
-time.sort()
+count = int(input())  # 사람의 수 입력
+time = list(map(int, input().split()))  # 사람 당 인출하는데 걸리는 시간 입력
+time.sort()  # 오름차순으로 정렬
 
 account = 0
 for i in range(count):
